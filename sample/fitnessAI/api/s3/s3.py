b549a5a638f9853926bbbb3624d0814fac319eb4
kevinouyang1 <ouyangkevin77@gmail.com>
2024-04-16 15:56:58 -0400

    Adding initial setup for API to interact with s3

---
+++ sample/fitnessAI/api/s3/s3.py
@@ -0,0 +1,33 @@
+import boto3
+from botocore.exceptions import ClientError
+from api.config import get_settings
+from datetime import datetime
+
+
+S3_REGION = 'us-east-2'
+UPLOAD_EXPIRY = 1800
+S3_BUCKET = 'gemini-fit-checks'
+
+settings = get_settings()
+
+s3_client = boto3.client(
+    "s3",
+    region_name = S3_REGION,
+    aws_access_key_id=settings.aws_access_key,
+    aws_secret_access_key=settings.aws_secret_key
+)
+
+def fetch_presigned_url(file_name):
+    now_time = datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
+
+    try:
+        presigned_url = s3_client.generate_presigned_post(
+            S3_BUCKET,
+            f'{file_name}/{now_time}',
+            ExpiresIn=UPLOAD_EXPIRY,
+        )
+    except ClientError as error:
+        print(error)
+        return None
+    
+    return presigned_url
